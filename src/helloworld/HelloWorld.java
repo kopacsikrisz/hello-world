3269e468b1c92ca2931e47cb4bd41ac867a7a668
Kopácsi Krisztián <kopacsi.krisztian@gmail.com>
2018-09-28 20:26:05 +0200

    Initial version of Hello World!

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+import java.io.Console;
+
+/**
+ *
+ * @author Kopácsi Krisztián
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+ 
+        System.out.println(extra.Console.readDouble()); 
+    }
+    
+}
